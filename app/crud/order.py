44d7ae3defa25a39fa40af631d8f8a9596e0aa02
Artem Nosov <103992767+avnosov3@users.noreply.github.com>
2023-09-05 20:41:28 +0200

    add get-all with names

--- app/crud/order.py
+++ app/crud/order.py
@@ -1,9 +1,30 @@
+from sqlalchemy import select
+
 from app.crud.base import CRUDBase
+from app.models.customer import Customer
 from app.models.order import Order
+from app.models.shopping_point import ShoppingPoint
+from app.models.worker import Worker
 
 
 class OrderCRUD(CRUDBase):
-    pass
+
+    async def get_all_with_names(self, session):
+        orders = await session.execute(
+            select(
+                Order.id,
+                Order.status,
+                Order.close_date,
+                Order.create_date,
+                Customer.name.label('customer'),
+                Worker.name.label('worker'),
+                ShoppingPoint.name.label('shopping_point')
+            )
+            .join(Customer, Order.customer_id == Customer.id)
+            .join(Worker, Order.worker_id == Worker.id)
+            .join(ShoppingPoint, Order.shopping_point_id == ShoppingPoint.id)
+        )
+        return orders.fetchall()
 
 
 order_crud = OrderCRUD(Order)
