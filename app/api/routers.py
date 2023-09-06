6d2c7baadfaa0f83ff7beff02e0b80835bc0667e
Artem Nosov <103992767+avnosov3@users.noreply.github.com>
2023-09-06 12:55:19 +0200

    add versions

--- app/api/routers.py
+++ app/api/routers.py
@@ -5,7 +5,7 @@ from app.api.order import order_router
 from app.api.shopping_point import shopping_point_router
 from app.api.visit import visit_router
 
-main_router_v1 = APIRouter()
+main_router_v1 = APIRouter(prefix='/api/v1')
 main_router_v1.include_router(autogenerate_router, prefix='/autogenerate', tags=['Autogenerate'])
 main_router_v1.include_router(shopping_point_router, prefix='/shopping-point', tags=['ShoppingPoint'])
 main_router_v1.include_router(order_router, prefix='/order', tags=['Order'])
